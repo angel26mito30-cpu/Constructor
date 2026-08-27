6c651a28951e6d775d713b322ff57a3e6d411ec1
Angel Rodriguez <angel26mito30@gmail.com>
2026-08-27 18:35:51 -0500

    creamos los metodos constructor

---
+++ src/main/java/clasedeobjetos/metodoconstructor/MetodoConstructor.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package clasedeobjetos.metodoconstructor;
+
+/**
+ *
+ * @author angel
+ */
+public class MetodoConstructor {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/clasedeobjetos/metodoconstructor/animalesVoladores.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clasedeobjetos.metodoconstructor;
+
+/**
+ *
+ * @author angel
+ */
+public class animalesVoladores {
+    
+    private String especie = "";
+    private int edad = 0;
+    private boolean macho = false;
+}
