1d7f5d2249ea368d491f9ea8a9cd343e4e68b062
Angel Rodriguez <angel26mito30@gmail.com>
2026-08-27 18:49:46 -0500

    ponemos los tres metodos constructores

--- src/main/java/clasedeobjetos/metodoconstructor/animalesVoladores.java
+++ src/main/java/clasedeobjetos/metodoconstructor/animalesVoladores.java
@@ -13,4 +13,22 @@ public class animalesVoladores {
     private String especie = "";
     private int edad = 0;
     private boolean macho = false;
+    
+    public animalesVoladores (String especie, int edad, boolean macho){
+        this.especie = especie;
+        this.edad = edad;
+        this.macho = macho;
+    }
+    
+    public animalesVoladores(int edad,String especie){
+        this.edad = edad;
+        this.especie = especie;
+    }
+    
+    public animalesVoladores(String especie, boolean macho){
+        this.especie = especie;
+        this.macho = macho;
+    }
+ 
 }
+ 
